0b4948374c57cd183598cd88173d829d2f18395a
Miguel <meolivaresa@gmail.com>
2015-12-03 19:14:21 +0000

    Fix heroku once again

--- core/migrations/0005_auto_20151029_1946.py
+++ core/migrations/0005_auto_20151029_1946.py
@@ -16,9 +16,13 @@ class Migration(migrations.Migration):
             name='name',
             field=models.CharField(max_length=300),
         ),
-        migrations.AlterField(
+        migrations.RemoveField(
             model_name='question',
             name='title',
-            field=models.IntegerField(default=0, choices=[(0, b''), (1, b'Jeb Bush'), (2, b'Ben Carson'), (3, b'Hillary Clinton'), (4, b'Ted Cruz'), (5, b'Lawrence Lessig'), (6, b'Martin OMalley'), (7, b'Marco Rubio'), (8, b'Bernie Sanders'), (9, b'Donald Trump')]),
+        ),
+migrations.AddField(
+            model_name='question',
+            name='title',
+            field=models.IntegerField(default=0, choices=[(0, b''), (1, b'Jeb Bush'), (2, b'Ben Carson'), (3, b'Hillary Clinton'), (4, b'Ted Cruz'), (5, b'Lawrence Lessig'), (6, b'Martin OMalley'), (7, b'Marco Rubio'), (8, b'Bernie Sanders'), (9, b'Donald Trump'), (10, b'Guido Lang')]),
         ),
     ]
